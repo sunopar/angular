8516e5eb5b577d46540cccd7f620755bf56faaed
opar-xu <opar.xu@gmail.com>
2025-02-10 10:12:36 +0400

    add ethereum provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { getProvider } from '@binance/w3w-ethereum-provider';
+console.log("🚀 ~~ W3wEthereumProvider:", getProvider)
 
 @Component({
   selector: 'app-root',
